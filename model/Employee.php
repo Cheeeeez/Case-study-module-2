0acb07c7f75394613c5ba62ab5d4e9078b441581
Nam <nguyentrongnama8@gmail.com>
2020-05-13 16:57:34 +0700

    ve giao dien login

---
+++ model/Employee.php
@@ -0,0 +1,62 @@
+<?php
+
+
+class Employee
+{
+    private $employeeId;
+    private $name;
+    private $gender;
+    private $email;
+    private $phone;
+    private $address;
+    private $position;
+
+    public function __construct($employeeId, $name, $gender, $email, $phone, $address, $position)
+    {
+        $this->employeeId = $employeeId;
+        $this->name = $name;
+        $this->gender = $gender;
+        $this->email = $email;
+        $this->phone = $phone;
+        $this->address = $address;
+        $this->position = $position;
+    }
+
+    public function getEmployeeId()
+    {
+        return $this->employeeId;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    public function getPosition()
+    {
+        return $this->position;
+    }
+
+
+}
+
