44bfe182d6b783dc1ba7962a5ac8c123816ede98
NguyenVanDuc1997 <nguyenvanduc160797@gmail.com>
2020-05-14 10:25:48 +0700

    model-update

--- model/EmployeeDB.php
+++ model/EmployeeDB.php
@@ -27,22 +27,39 @@ class EmployeeDB
     public function getByID($id)
     {
         $sql = "SELECT * FROM employees WHERE emp_id = ?";
-        $statement= $this->conn->prepare($sql);
-        $statement->bindParam(1,$id);
+        $statement = $this->conn->prepare($sql);
+        $statement->bindParam(1, $id);
         $statement->execute();
-        $row= $statement->fetch();
-        $employee= new Employee($row['emp_name'],$row['email'],$row['phone'],$row['address'],$row['gender'],$row['pos_id']);
-        $employee->emp_id= $row['emp_id'];
+        $row = $statement->fetch();
+        $employee = new Employee($row['emp_name'], $row['email'], $row['phone'], $row['address'], $row['gender'],
+            $row['pos_id']);
+        $employee->emp_id = $row['emp_id'];
         return $employee;
     }
+
     //delete by id
-    public function deleteById($id){
-        $sql= "DELETE FROM employees WHERE emp_id = ?";
-        $statement= $this->conn->prepare($sql);
-        $statement->bindParam(1,$id);
+    public function deleteById($id)
+    {
+        $sql = "DELETE FROM employees WHERE emp_id = ?";
+        $statement = $this->conn->prepare($sql);
+        $statement->bindParam(1, $id);
         return $statement->execute();
     }
 
+    //update employee
+    public function updateById($id, $employee)
+    {
+        $sql = "UPDATE employees SET emp_name = ?,email = ?,phone = ? ,address= ?, gender = ?,pos_id = ? WHERE emp_id = ?";
+        $statement = $this->conn->prepare($sql);
+        $statement->bindParam(1, $employee->getName());
+        $statement->bindParam(2, $employee->getEmail());
+        $statement->bindParam(3, $employee->getPhone());
+        $statement->bindParam(4, $employee->getAddress());
+        $statement->bindParam(5, $employee->getGender());
+        $statement->bindParam(6, $employee->getPosition());
+        $statement->bindParam(7, $employee->$id);
+        return $statement->execute();
+    }
 
     public function getAll()
     {
